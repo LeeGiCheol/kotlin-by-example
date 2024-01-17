bcf65e9e5af14300cc2f23de1fb946ac4e8fa0e4
LeeGiCheol <leegicheolgc@gmail.com>
2024-01-18 00:02:39 +0900

    [init] project init

---
+++ src/main/kotlin/org/example/kotlinbyexample/KotlinByExampleApplication.kt
@@ -0,0 +1,11 @@
+package org.example.kotlinbyexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinByExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinByExampleApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/kotlinbyexample/KotlinByExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.kotlinbyexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinByExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
